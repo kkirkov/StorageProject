cb3623f7b1606f17a58b74586060c18af1db37d8
Kristin <krischanss99@gmail.com>
2020-05-04 12:06:33 +0300

    03.05 changes

--- Project.cpp
+++ Project.cpp
@@ -4,8 +4,9 @@ using namespace std;
 class Product
 {
 private:
+public:
     char* name;
-    unsigned int timeToExp;
+    int timeToExp;
     char* dateOfEntry;
     char* supplier;
     char* unit;
@@ -17,7 +18,7 @@ public:
     {
 
     }
-    Product(char* name, unsigned int timeToExp,char* dateOfEntry, char* supplier, char* unit, double amount, unsigned int location, char* note)
+    Product(char* name, int timeToExp,char* dateOfEntry, char* supplier, char* unit, double amount, unsigned int location, char* note)
     {
         this->name = name;
         this->timeToExp = timeToExp;
@@ -28,11 +29,95 @@ public:
         this->location = location;
         this->note = note;
     }
+        Product(char* name, int timeToExp,char* dateOfEntry, char* supplier, char* unit, double amount, unsigned int location)
+    {
+        this->name = name;
+        this->timeToExp = timeToExp;
+        this->dateOfEntry = dateOfEntry;
+        this->supplier = supplier;
+        this->unit = unit;
+        this->amount = amount;
+        this->location = location;
+    }
+            Product(char* name, int timeToExp,char* dateOfEntry, char* supplier, char* unit, double amount)
+    {
+        this->name = name;
+        this->timeToExp = timeToExp;
+        this->dateOfEntry = dateOfEntry;
+        this->supplier = supplier;
+        this->unit = unit;
+        this->amount = amount;
+    }
+    void setLocation(unsigned int location)
+    {
+        this->location=location;
+    }
+    unsigned int getAmount()
+    {
+        return amount;
+    }
+
 
 };
+Product* kokakola [2][2];
+bool mirrorTable [2][2];
+
+void FillMirrorTable()
+{
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            mirrorTable[i][j] = false;
+        }
+    }
+}
+unsigned int SearchForFreeSpace(unsigned int spaceNeeded)
+{
+    unsigned int currentSpace;
+    for(int i=0;i<2;i++)
+    {
+        currentSpace = 0;
+        //За да сме сигурни, че продуктът ни е за един ред
+        for(int j=0;j<2;j++)
+        {
+            if(mirrorTable[i][j] = false)
+            {
+                currentSpace++;
+                if(currentSpace==spaceNeeded)
+                {
+                    return i*100+(j-spaceNeeded);
+                }
+            }
+            else currentSpace = 0;
+        }
+    }
+
+}
+void PutTheProduct(unsigned int positionToPlaceOn,unsigned int spaceNeeded)
+{
+    for(int i=0;i<spaceNeeded;i++)
+    {
+        mirrorTable[positionToPlaceOn/100][positionToPlaceOn%100 + i] = true;
+    }
+}
+void AddNewProduct(Product* newProduct)
+{
+    unsigned int spaceNeeded = newProduct->getAmount()/50;
+    unsigned int positionToPlaceOn = SearchForFreeSpace(spaceNeeded);
+    newProduct->setLocation(positionToPlaceOn);
+    kokakola[positionToPlaceOn/100][positionToPlaceOn%100] = newProduct;
+    PutTheProduct(positionToPlaceOn,spaceNeeded);
+
+}
 
 
 int main()
 {
-
+    FillMirrorTable();
+    Product* kola  = new Product("kola",30,"30/4/20","koka-kola","0.5L",24,1001);
+    Product* fanta  = new Product("fanta",30,"30/4/20","koka-kola","0.5L",12,1002);
+    kokakola[0][0] = kola;
+    kokakola[0][1] = fanta;
+cout<<kokakola[0][1]->name<<endl;
 };
